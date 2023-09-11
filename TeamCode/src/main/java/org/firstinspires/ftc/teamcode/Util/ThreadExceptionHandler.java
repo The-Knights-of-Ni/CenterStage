6244410dbcc0918674ae08936474f368a7eefc6f
Ashwin Naren <arihant2math@gmail.com>
2023-09-10 17:31:41 -0700

    IMPORTANT: Syntax bug fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/ThreadExceptionHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/ThreadExceptionHandler.java
@@ -2,15 +2,13 @@ package org.firstinspires.ftc.teamcode.Util;
 
 import android.util.Log;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.jetbrains.annotations.NotNull;
-
 import java.lang.Thread.UncaughtExceptionHandler;
 
 public class ThreadExceptionHandler implements UncaughtExceptionHandler {
 
     Telemetry telemetry;
     @Override
-    public void uncaughtException(Thread t, @NotNull Throwable e) {
+    public void uncaughtException(Thread t, Throwable e) {
         telemetry.addData("Thread Death Message: ","Caught Exception on Thread " + t.getClass().getName() + "\nThread shutting down\nSee logcat for details");
         Log.e("main", "Caught Exception on Thread " + t.getId() + " " + t.getClass().getName() + "\nThread shutting down", e);
         t.interrupt();
