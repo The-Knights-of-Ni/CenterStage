0b45ac0d302bc59631176d4956b18dc28b68a1d7
Ashwin Naren <arihant2math@gmail.com>
2023-09-16 17:20:58 -0700

    fixed the autos :)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoBlueRight.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import org.firstinspires.ftc.teamcode.Util.AllianceColor;
+import org.firstinspires.ftc.teamcode.Util.Vector;
+import org.firstinspires.ftc.teamcode.Subsystems.Vision.*;
+
+public class AutoBlueRight extends Auto{
+
+    public void runOpMode() {
+        initAuto(AllianceColor.BLUE);
+        robot.drive.moveVector(new Vector(24*mmPerInch, 0));
+        MarkerDetectionPipeline.MarkerLocation markerPosition = robot.vision.detectMarkerRun();
+        robot.vision.stop();
+        waitForStart();
+        timer.reset();
+        robot.drive.moveVector(new Vector(20 * mmPerInch, 0));
+        // Place pixel on tape
+        robot.drive.moveAngle(-90);
+        robot.drive.moveVector(new Vector(0, 56 * mmPerInch));
+        //Detect apriltags
+        //Place pixel on backdrop
+        // Move out of the way
+    }
+
+}
