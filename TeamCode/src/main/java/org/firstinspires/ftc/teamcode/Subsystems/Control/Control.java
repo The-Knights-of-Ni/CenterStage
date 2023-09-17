1c7e244b64435f35ca1b7fb6c7bbca060d5758fa
albonec <agbonecchi@gmail.com>
2023-09-16 21:58:17 -0700

    inital control stuff fleshed out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Control/Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Control/Control.java
@@ -1,9 +1,6 @@
 package org.firstinspires.ftc.teamcode.Subsystems.Control;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Servo;
-
 import org.firstinspires.ftc.teamcode.Subsystems.Subsystem;
 
 
@@ -16,6 +13,50 @@ public class Control extends Subsystem {
         super(telemetry, "control");
     }
 
+    enum DoubleGrippyState { //TODO: Calibrate claw constants
+        CLOSE_FRONT(0,0),
+        CLOSE_REAR(0,0),
+        OPEN_FRONT(0,0),
+        OPEN_REAR(0,0),
+        OPEN_SIMUL(0,0),
+        CLOSE_SIMUL(0,0);
+        private double posFront;
+        private double posRear;
+        DoubleGrippyState(double posFront, double posRear) {
+            this.posFront = posFront;
+            this.posRear = posRear;
+        }
+    }
+
+    enum PlaneLaunchRange { //TODO: Calibrate Motor Powers
+        LONG(0),
+        MEDIUM(0),
+        SHORT(0);
+        private double motorPower;
+        PlaneLaunchRange(double motorPower) {
+            this.motorPower = motorPower;
+        }
+    }
+
+    enum SlidePosition { //TODO: Calibrate Slide Constants
+        EXTENDED(0),
+        RETRACTED(0),
+        OBJ_PICKUP(0);
+        private double pos;
+        SlidePosition(double pos) {
+            this.pos = pos;
+        }
+    }
+
+    enum CraneState { //TODO: Calibrate crane constants
+        UP(0),
+        DOWN(0);
+        private double tickCount;
+        CraneState(double tickCount) {
+            this.tickCount = tickCount;
+        }
+    }
+
     public void initDevicesAuto() {
 
     }
@@ -23,4 +64,21 @@ public class Control extends Subsystem {
     public void initDevicesTeleop() {
 
     }
+
+    public void moveDoubleGrippy(DoubleGrippyState pos) {
+        //move claw thing
+    }
+
+    public void craneLift(CraneState state) {
+        //Crane operation
+    }
+
+    public void airplaneLaunch(PlaneLaunchRange range) {
+        //launch plane
+    }
+
+    public void moveLinearSlide(SlidePosition pos) {
+        //Move linear slide up and down
+    }
+
 }
