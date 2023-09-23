7d1cbd35c3d71631f508c4238c60ed709cd4613d
Orcasphynx <richardwilliamcrouch@gmail.com>
2023-09-23 14:07:02 -0700

    auto blue right

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoBlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoBlueRight.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
+import org.firstinspires.ftc.teamcode.Subsystems.Control.Control;
 import org.firstinspires.ftc.teamcode.Util.AllianceColor;
 import org.firstinspires.ftc.teamcode.Util.Vector;
 import org.firstinspires.ftc.teamcode.Subsystems.Vision.*;
@@ -8,13 +9,39 @@ public class AutoBlueRight extends Auto{
 
     public void runOpMode() {
         initAuto(AllianceColor.BLUE);
-        robot.drive.moveVector(new Vector(24*mmPerInch, 0));
         MarkerDetectionPipeline.MarkerLocation markerPosition = robot.vision.detectMarkerRun();
         robot.vision.stop();
         waitForStart();
         timer.reset();
-        robot.drive.moveVector(new Vector(20 * mmPerInch, 0));
         // Place pixel on tape
+        switch (markerPosition) {
+            case LEFT:
+                robot.drive.moveVector(new Vector(12*mmPerInch, 24 * mmPerInch), -90);
+                robot.control.moveLinearSlide(Control.SlidePosition.EXTENDED);
+                robot.control.moveDoubleGrippy(Control.DoubleGrippyState.OPEN_FRONT);
+                robot.control.moveLinearSlide(Control.SlidePosition.RETRACTED);
+                robot.drive.moveVector(new Vector(0, 60 * mmPerInch));
+
+
+                break;
+            case MIDDLE:
+                robot.drive.moveVector(new Vector(12 * mmPerInch, 0));
+                robot.control.moveLinearSlide(Control.SlidePosition.EXTENDED);
+                robot.control.moveDoubleGrippy(Control.DoubleGrippyState.OPEN_FRONT);
+                robot.control.moveLinearSlide(Control.SlidePosition.RETRACTED);
+                robot.drive.moveVector(new Vector(0,0),-90);
+                robot.drive.moveVector(new Vector(0, 60 * mmPerInch));
+                break;
+            case RIGHT:
+                robot.drive.moveVector(new Vector(12 * mmPerInch, 0),90);
+                robot.control.moveLinearSlide(Control.SlidePosition.EXTENDED);
+                robot.control.moveDoubleGrippy(Control.DoubleGrippyState.OPEN_FRONT);
+                robot.control.moveLinearSlide(Control.SlidePosition.RETRACTED);
+                robot.drive.moveVector(new Vector(0,0), -180);
+                robot.drive.moveVector(new Vector(60,0));
+
+                break;
+        }
         robot.drive.moveAngle(-90);
         robot.drive.moveVector(new Vector(0, 56 * mmPerInch));
         //Detect apriltags
