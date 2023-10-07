d98531390c0875772d4adc89319bc04421e1ede4
Ashwin Naren <arihant2math@gmail.com>
2023-10-07 14:52:34 -0700

    imu logging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/MasterLogger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/MasterLogger.java
@@ -9,27 +9,37 @@ public class MasterLogger {
     String tag;
 
     public MasterLogger(Telemetry telemetry, String tag) {
-        this.telemetry = telemetry;
+        if (telemetry != null) {
+            this.telemetry = telemetry;
+        } else {
+            this.telemetry = null;
+        }
         this.tag = tag;
     }
 
     public void error(String message) {
-        telemetry.addData(tag, message);
-        telemetry.update();
+        if (telemetry != null) {
+            telemetry.addData(tag, message);
+            telemetry.update();
+        }
         Log.e(tag, message);
         WebLog.error(tag, message);
     }
 
     public void error(String message, Throwable tr) {
-        telemetry.addData(tag, message);
-        telemetry.update();
+        if (telemetry != null) {
+            telemetry.addData(tag, message);
+            telemetry.update();
+        }
         Log.e(tag, message, tr);
         WebLog.error(tag, message);
     }
 
     public void warning(String message) {
-        telemetry.addData(tag, message);
-        telemetry.update();
+        if (telemetry != null) {
+            telemetry.addData(tag, message);
+            telemetry.update();
+        }
         Log.w(tag, message);
         WebLog.warning(tag, message);
     }
