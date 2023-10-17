d4a94d6b61a0f4d32f276eb0ed9dd476cdeb2835
Ashwin Naren <arihant2math@gmail.com>
2023-10-17 16:18:21 -0700

    minor changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -100,6 +100,9 @@ public class Robot {
         return result;
     }
 
+    /**
+     * Runs all init operations
+     */
     public void init() {
         motorInit();
         servoInit();
@@ -147,6 +150,9 @@ public class Robot {
         }
     }
 
+    /**
+     * Gets Motors from hardware ap and sets zero power behavior and direction
+     */
     private void motorInit() {
         frontLeftDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("fl");
         frontRightDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("fr");
