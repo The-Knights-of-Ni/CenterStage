7145802081e5fb1dc41c38861cd213413ccba8b6
moogloof <ethansyun@icloud.com>
2023-11-04 08:35:47 -0700

    red or blue detect

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision/MarkerDetectionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision/MarkerDetectionPipeline.java
@@ -69,8 +69,15 @@ public class MarkerDetectionPipeline extends OpenCvPipeline {
             markerLocation = MarkerLocation.NOT_FOUND;
             return input;
         }
-        Scalar lowHSV = new Scalar(70.0, 100.0, 100.0);
-        Scalar highHSV = new Scalar(85.0, 255.0, 235.0);
+
+        // Blue alliance
+        Scalar lowHSV = new Scalar(138.0, 100.0, 100.0);
+        Scalar highHSV = new Scalar(174.0, 255.0, 235.0);
+        // Or red
+        if (allianceColor == AllianceColor.RED) {
+            lowHSV = new Scalar(233.0, 100.0, 100.0);
+            highHSV = new Scalar(255.0, 255.0, 235.0);
+        }
         Mat thresh = new Mat();
 
         Core.inRange(crop, lowHSV, highHSV, thresh);
