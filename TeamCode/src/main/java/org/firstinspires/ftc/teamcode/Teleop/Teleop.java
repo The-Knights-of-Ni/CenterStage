745119cb9a488b0b437291d3216902bc57463464
moogloof <ethansyun@icloud.com>
2023-11-06 15:36:36 -0800

    No web in teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop.java
@@ -25,7 +25,7 @@ public class Teleop extends LinearOpMode {
         // Initialize DC motor objects
         timer = new ElapsedTime();
         HashMap<String, Boolean> flags = new HashMap<>();
-        flags.put("web", true);
+        flags.put("web", false);
         flags.put("vision", false);
         this.robot = new Robot(hardwareMap, telemetry, timer, AllianceColor.BLUE, gamepad1, gamepad2, flags);
         timeCurrent = timer.nanoseconds();
