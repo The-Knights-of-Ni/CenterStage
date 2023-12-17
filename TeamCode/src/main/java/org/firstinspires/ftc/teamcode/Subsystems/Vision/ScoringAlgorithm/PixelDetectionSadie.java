81a6786e75872668188ccd8fab0aa5656cccc600
crazyGyroSadie <116518784+crazyGyroSadie@users.noreply.github.com>
2023-12-17 18:04:19 -0500

    Early stages of pixel detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision/ScoringAlgorithm/PixelDetectionSadie.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision/ScoringAlgorithm/PixelDetectionSadie.java
@@ -1,2 +1,27 @@
-package org.firstinspires.ftc.teamcode.Subsystems.Vision.ScoringAlgorithm;public class PixelDetectionSadie {
+package org.firstinspires.ftc.teamcode.Subsystems.Vision.ScoringAlgorithm;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.opencv.*;
+import org.opencv.imgproc.Imgproc;
+
+public class PixelDetectionSadie extends OpenCvPipeline
+{
+    @Override
+    public Mat processFrame(Mat input)
+    {
+        Mat grayscale = new Mat();
+        Imgproc.cvtColor(input, grayscale, Imgproc.COLOR_BGR2GRAY);
+        input.release();
+        Mat threshold = new Mat();
+        Imgproc.threshold(grayscale, threshold, 0, 255, Imgproc.THRESH_BINARY_INV + Imgproc.THRESH_OTSU);
+
+
+        return null;
+    }
+
+    /*public String detectShape(Mat c)
+    {
+
+    }*/
 }
