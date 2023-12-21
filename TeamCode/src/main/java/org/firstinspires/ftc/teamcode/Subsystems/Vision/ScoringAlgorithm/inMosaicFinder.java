73bf3327c63513b2939ceab2963e1a9f5441e494
Orcasphynx <richardwilliamcrouch@gmail.com>
2023-12-21 14:02:37 -0500

    Merge remote-tracking branch 'origin/control' into control
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drive/Drive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision/ScoringAlgorithm/inMosaicFinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Vision/ScoringAlgorithm/inMosaicFinder.java
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.teamcode.Subsystems.Vision.ScoringAlgorithm;
 
 public class inMosaicFinder {
-    public boolean inMosaic(){
+    //leftmost and rightmost pixel of long row
+    public boolean inMosaic_Case1(Pixel thepixel, Pixel top, Pixel side, Pixel bottom){
         return true;
     }
 }
