cd876fa1afeac1740390197726be0f084e05b23b
Ashwin Naren <arihant2math@gmail.com>
2024-01-05 14:41:59 -0800

    Two command drive test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testop/Drive/TwoCommandDriveTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.Testop.Drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.Auto.Auto;
+import org.firstinspires.ftc.teamcode.Util.AllianceColor;
+import org.firstinspires.ftc.teamcode.Util.Vector;
+
+@Autonomous(name = "Forward Test", group = "Concept")
+public class TwoCommandDriveTest extends Auto {
+    /**
+     * Override of runOpMode()
+     *
+     * <p>Please do not swallow the InterruptedException, as it is used in cases where the op mode
+     * needs to be terminated early.</p>
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+     */
+    @Override
+    @SuppressWarnings("RedundantThrows")
+    public void runOpMode() throws InterruptedException {
+        initAuto(AllianceColor.RED);
+        waitForStart();
+        timer.reset();
+        robot.drive.moveVector(new Vector(0 * mmPerInch, 24 * mmPerInch));
+        robot.drive.moveVector(new Vector(24 * mmPerInch, 0 * mmPerInch));
+        sleep(2000);
+    }
+}
