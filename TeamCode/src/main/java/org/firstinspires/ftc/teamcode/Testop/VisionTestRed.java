20d819bc438c811822c4bf2417d3374a5cd51286
Ashwin Naren <arihant2math@gmail.com>
2024-01-30 19:10:56 -0800

    added vision test red

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testop/VisionTestRed.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.Testop;
+
+import android.os.Build;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.Auto.Auto;
+import org.firstinspires.ftc.teamcode.Subsystems.Vision.MarkerDetectionPipeline;
+import org.firstinspires.ftc.teamcode.Util.AllianceColor;
+
+@Autonomous(name = "Vision Test Red", group = "Concept")
+public class VisionTestRed extends Auto {
+    /**
+     * Override of runOpMode()
+     *
+     * <p>Please do not swallow the InterruptedException, as it is used in cases where the op mode
+     * needs to be terminated early.</p>
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+     */
+    @Override
+    @SuppressWarnings("RedundantThrows")
+    public void runOpMode() throws InterruptedException {
+        initAuto(AllianceColor.RED);
+        waitForStart();
+        MarkerDetectionPipeline.MarkerLocation markerPosition = robot.vision.detectMarkerRun();
+        timer.reset();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Marker Position: ", markerPosition.name());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                Thread.onSpinWait();
+            } else {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
