e37609c09e92800c0c53eca287d63ae661b9747b
Ashwin Naren <arihant2math@gmail.com>
2024-02-01 16:52:55 -0800

    bugfix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drive/OldDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drive/OldDrive.java
@@ -96,7 +96,7 @@ public class OldDrive extends Subsystem {
     }
 
     public static boolean isMotorDone(int currentCount, int targetCount) {
-        return Math.abs(targetCount) - Math.abs(currentCount)  > 0;
+        return Math.abs(currentCount - targetCount) < 25;
     }
 
     /**
